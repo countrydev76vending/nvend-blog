f60057290204da2be25f9736d577858da6c791f6
magmajo <countrydev76vending@gmail.com>
2019-03-14 16:14:21 +0900

    covert favicon color to gray

---
+++ hjjo_start.sh
@@ -0,0 +1,10 @@
+## http://themes.jekyllrc.org/
+## http://themes.jekyllrc.org/TeXt/
+
+#wget https://github.com/kitian616/jekyll-TeXt-theme/archive/master.zip
+#mv master.zip jekyll-TeXt-theme-master.zip
+#unzip jekyll-TeXt-theme-master.zip
+#mv jekyll-TeXt-theme-master nblog-countrydev
+#cd nblog-countrydev
+
+bundle exec jekyll serve
